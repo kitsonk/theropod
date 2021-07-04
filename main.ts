520557c51c74910a0d71053977210501ba4447f2
Kitson Kelly <me@kitsonkelly.com>
2021-07-04 14:14:56 +1000

    fix xhr

--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
-// import "https://deno.land/x/xhr@0.1.0/mod.ts";
-import "./xhr.ts";
+import "https://deno.land/x/xhr@0.1.1/mod.ts";
 
 // @deno-types=https://cdn.skypack.dev/-/firebase@v8.7.0-MrU9zUCxcEMCl2U7Tuz6/dist=es2020,mode=types/index.d.ts
 import firebase from "https://cdn.skypack.dev/firebase@8.7.0/app";
