5f85cae4afd7ed7aa0d4e411fd689623e5a0db06
Kitson Kelly <me@kitsonkelly.com>
2021-07-05 11:08:53 +1000

    refactor

--- main.ts
+++ main.ts
@@ -8,34 +8,19 @@ import "https://cdn.skypack.dev/firebase@8.7.0/firestore";
 import * as colors from "https://deno.land/std@0.100.0/fmt/colors.ts";
 import { Application, Router } from "https://deno.land/x/oak@v7.7.0/mod.ts";
 
-// This is a "hack" to take local storage under Deploy and set them as cookies
-// in the client.  Currently, I am using oak's cookie management to actually
-// save and restore the keys and values.
-import { Storage } from "./cookieStorage.ts";
+// I created a small library called of virtual storage which provides the
+// DOM storage API and allows state to be saved and stored externally, and then
+// create a oak middleware that takes that state and saves it as cookies.  This
+// allows the firebase auth module to persist the authentication of the user to
+// the cookies of the user and have those be restored.
+import { installGlobals } from "./virtualStorage.ts";
+import { virtualStorage } from "./virtualStorageMiddleware.ts";
 
-// We only do this if we don't have session storage.
-const sessionStore = new Storage();
-if (!("sessionStorage" in globalThis)) {
-  Object.defineProperty(globalThis, "sessionStorage", {
-    value: sessionStore,
-    writable: false,
-    enumerable: true,
-    configurable: true,
-  });
-}
+// Storage globals need to be installed prior to the application auth being
+// setup.
+installGlobals({ overwrite: true });
 
 const users = new Map<string, firebase.User>();
-
-// We only do this if we don't have local storage.
-const localStore = new Storage();
-if (!("localStorage" in globalThis)) {
-  Object.defineProperty(globalThis, "localStorage", {
-    value: localStore,
-    writable: false,
-    enumerable: true,
-    configurable: true,
-  });
-}
 
 // This is the "client" initialization keys, these end up in a client
 // un-encrypted but you still need a login to the app to do anything.
@@ -50,6 +35,8 @@ const theropod = firebase.initializeApp({
 
 // This gets a handle to the auth part
 const auth = firebase.auth(theropod);
+// This is implied, but wanted to make it explicit, that we are setting the
+// persistance to local storage.
 auth.setPersistence("local");
 
 const db = firebase.firestore(theropod);
@@ -61,12 +48,8 @@ router.get("/", (ctx) => {
 });
 
 router.get("/users", async (ctx) => {
-  console.log("/users");
-  const start = Date.now();
   const querySnapshot = await db.collection("users").get();
-  console.log("post snapshot", Date.now() - start);
   ctx.response.body = querySnapshot.docs.map((doc) => doc.data());
-  console.log("doc map", Date.now() - start);
   ctx.response.type = "json";
 });
 
@@ -74,6 +57,7 @@ const app = new Application({
   keys: JSON.parse(Deno.env.get("THEROPOD_KEYS") ?? `["secret"]`),
 });
 
+// A basic logging middleware.
 app.use(async (ctx, next) => {
   await next();
   const rt = ctx.response.headers.get("X-Response-Time");
@@ -84,6 +68,7 @@ app.use(async (ctx, next) => {
   );
 });
 
+// A basic timing middleware.
 app.use(async (ctx, next) => {
   const start = Date.now();
   await next();
@@ -91,48 +76,9 @@ app.use(async (ctx, next) => {
   ctx.response.headers.set("X-Response-Time", `${ms}ms`);
 });
 
-// this is the middleware that takes local-storage to and from cookies, it
-// hydrates the store based on `"TP_KEYS"` and then when the rest of the
-// middleware has run, it sets or deletes any values that have changed.
-// It is important to base64 encode the values, as we need to make sure the
-// values don't contain anything that might look like a cookie argument, which
-// often values do.
-app.use(async (ctx, next) => {
-  console.log(":localStorage");
-  const localStorageKeysStr = ctx.cookies.get("TP_KEYS");
-  if (localStorageKeysStr) {
-    try {
-      const keys: string[] = JSON.parse(localStorageKeysStr);
-      const entries: [string, string][] = [];
-      for (const key of keys) {
-        const value = ctx.cookies.get(`TP_${key}`);
-        if (value) {
-          entries.push([key, atob(value)]);
-        }
-        localStore.hydrate(entries);
-      }
-    } catch {
-      // we just swallow errors here
-    }
-  }
-  await next();
-  console.log(":post next");
-  const keys = [...localStore.keys()];
-  if (keys.length) {
-    const value = JSON.stringify(keys);
-    if (value !== localStorageKeysStr) {
-      ctx.cookies.set("TP_KEYS", JSON.stringify(keys), { overwrite: true });
-    }
-    for (const key of localStore.keysSet()) {
-      ctx.cookies.set(`TP_${key}`, btoa(localStore.getItem(key) ?? ""), {
-        overwrite: true,
-      });
-    }
-  }
-  for (const key of localStore.keysDeleted()) {
-    ctx.cookies.delete(`TP_${key}`, { overwrite: true });
-  }
-});
+// This is the middleware that stores `localStorage` to cookies on the request
+// and restores them on subsequent requests.
+app.use(virtualStorage());
 
 // this middleware logs in the user if there isn't a cookie that logs them in
 // we have to set a flag, otherwise firebase things we are trying to re-auth a
@@ -140,17 +86,11 @@ app.use(async (ctx, next) => {
 // and the local storage has the auth credentials in local storage, the other
 // API calls work just fine.
 app.use(async (ctx, next) => {
-  console.log(":auth");
   // The default persistance is `local` which uses `localStorage` to save the
   // login.
   const signedInUid = ctx.cookies.get("TP_UID");
   const signedInUser = signedInUid != null ? users.get(signedInUid) : undefined;
-  console.log(
-    `cookie: ${signedInUid}, user: ${signedInUser?.uid}, current: ${auth
-      .currentUser?.uid}`,
-  );
   if (!signedInUid || !signedInUser || !auth.currentUser) {
-    console.log("sign-in");
     const creds = await auth.signInWithEmailAndPassword(
       Deno.env.get("THEROPOD_USERNAME")!,
       Deno.env.get("THEROPOD_PASSWORD")!,
@@ -161,7 +101,6 @@ app.use(async (ctx, next) => {
       ctx.cookies.set("TP_UID", user.uid);
     }
   } else if (signedInUser && signedInUser.uid !== auth.currentUser?.uid) {
-    console.log("swap user");
     await auth.updateCurrentUser(signedInUser);
   }
   return next();
@@ -170,6 +109,8 @@ app.use(async (ctx, next) => {
 app.use(router.routes());
 app.use(router.allowedMethods());
 
+// Should log any internal errors that are thrown within the oak application
+// with some context that should make isolating the problem better
 app.addEventListener("error", (evt) => {
   let msg = `[${colors.red("error")}] `;
   if (evt.error instanceof Error) {
